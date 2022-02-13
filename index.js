7c8b4ae60c689f5c9b0923b2f21560e56651f2b0
Max Pietsch <max.pietsch@hotmail.com>
2022-02-13 11:06:05 +0100

    mobile navbar style

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const navContainer = document.querySelector(".toggle-navbar-container");
 
 burger.addEventListener("click", () => {
   navContainer.classList.add("toggle-navbar-container-active");
-  console.log(navContainer);
 });
 
 navContainer.addEventListener("click", () => {
-  console.log(navContainer);
   navContainer.classList.remove("toggle-navbar-container-active");
 });

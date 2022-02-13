a3d3f987bb01b5909afd23b0b929e407e4311b00
Max Pietsch <max.pietsch@hotmail.com>
2022-02-13 11:22:33 +0100

    stop scroll function

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const burger = document.querySelector(".nav-burger");
 const navContainer = document.querySelector(".toggle-navbar-container");
+const body = document.querySelector("body");
+console.log(body);
 
 burger.addEventListener("click", () => {
   navContainer.classList.add("toggle-navbar-container-active");
+  body.classList.add("stop-scroll");
 });
 
 navContainer.addEventListener("click", () => {
   navContainer.classList.remove("toggle-navbar-container-active");
+  body.classList.remove("stop-scroll");
 });

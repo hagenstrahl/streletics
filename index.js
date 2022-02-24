4698137b744d38d430a3e61ae1d183f920424429
Max Pietsch <max.pietsch@hotmail.com>
2022-02-24 09:58:41 +0100

    add impressum and links

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector(".nav-burger");
 const navContainer = document.querySelector(".toggle-navbar-container");
 const body = document.querySelector("body");
-console.log(body);
+const impressumContainer = document.querySelector(".impressum-container");
+const impressumLink = document.querySelector("#impressum");
 
+//Mobile navbar
 burger.addEventListener("click", () => {
   navContainer.classList.add("toggle-navbar-container-active");
   body.classList.add("stop-scroll");
@@ -11,4 +13,12 @@ burger.addEventListener("click", () => {
 navContainer.addEventListener("click", () => {
   navContainer.classList.remove("toggle-navbar-container-active");
   body.classList.remove("stop-scroll");
+});
+
+// Impressum
+impressumLink.addEventListener("click", () => {
+  impressumContainer.classList.toggle("impressum-active");
+});
+impressumContainer.addEventListener("click", () => {
+  impressumContainer.classList.remove("impressum-active");
 });
